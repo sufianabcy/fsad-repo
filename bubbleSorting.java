6ddbb392ebdf43dd137acf5a578781d87db3a4f5
sufianabcy <abu966671@gmail.com>
2026-01-08 10:12:23 +0530

    Adding bubble sort algorithm and cyclic sort algorithm

---
+++ bubbleSorting.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class bubbleSorting{
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        bubbly(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubbly(int[] arr){
+        boolean swapped;
+        //run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped=false;
+            //for each index, max items will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
---
+++ cyclicSorting.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class cyclicSorting {
+    public static void main(String[] args) {
+        int[] arr={5,4,2,3,1};
+        cyclicSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclicSort(int[] arr) {
+        int i=0;
+        while(i < arr.length){
+            int correctIndex = arr[i]-1;
+            //here we want to check if the index we are checking i.e "i" index == the value in i-1 (correct index)
+            //means we want to know if 3 that is in i index is in arr[i]-1 ==2 so 3 should be in 2 index i.e i should be 2 or else swap
+            if (arr[i] != arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr, int first , int second){
+        int temp= arr[first];
+        arr[first]= arr[second];
+        arr[second]= temp;
+    }
+}
